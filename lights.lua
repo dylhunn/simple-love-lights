ca30aa11c461ee0a09f3363c7e6baba2443c9fa9
pakeke-constructor <62230683+pakeke-constructor@users.noreply.github.com>
2020-03-24 23:54:46 +1300

    Update lights.lua
    Made variables local. 
    User can now do::
    
    local light = require 'lights.lua' 
    and access functions from there.
    
    (instead of having globals willy nilly everywhere.)
    Also fixed x,y being global in one of the functions- was bound to cause problems at some point! : )

--- lights.lua
+++ lights.lua
@@ -1,3 +1,4 @@
+local LG = love.graphics
 -- This library is a trivial implementation of raycasting point lights for love2d/LÖVE.
 -- It is heavily based on mattdesl's libGDX implementation, described here:
 --   https://github.com/mattdesl/lwjgl-basics/wiki/2D-Pixel-Perfect-Shadows
@@ -5,13 +6,16 @@
 -- The light data is stored here.
 local lightInfos = {}
 
+-- Table to be returned.
+local light = {}
+
 ----------------
 -- PUBLIC API --
 ----------------
 
 -- Call this function to add a new light; provide the absolute coordinates, size of
 -- the illuminated containing box, and color.
-function addLight(lx, ly, lsize, lr, lg, lb)
+function light.addLight(lx, ly, lsize, lr, lg, lb)
 
 	-- Don't allow multiple lights at the same point.
 	for _, li in ipairs(lightInfos) do
@@ -19,103 +23,102 @@ function addLight(lx, ly, lsize, lr, lg, lb)
 	end
 
 	table.insert(lightInfos, {x=lx, y=ly, size=lsize, r=lr, g=lg, b=lb,
-		occludersCanvas = love.graphics.newCanvas(lsize, lsize),
-		shadowMapCanvas = love.graphics.newCanvas(lsize, 1),
-		lightRenderCanvas = love.graphics.newCanvas(lsize, lsize),
+		occludersCanvas = LG.newCanvas(lsize, lsize),
+		shadowMapCanvas = LG.newCanvas(lsize, 1),
+		lightRenderCanvas = LG.newCanvas(lsize, lsize),
 	})
 end
 
 -- Clear all lights.
-function clearLights()
+function light.clearLights()
 	lightInfos = {}
 end
 
 -- You must call this from the main draw function, before drawing other objects.
 -- Pass in a function that draws all shadow-casting objects to the screen.
 -- Also pass in the coordinate transformation from absolute coordinates.
-function drawLights(drawOccludersFn, coordTransX, coordTransY)
+function light.drawLights(drawOccludersFn, coordTransX, coordTransY)
 	for i = 1, #lightInfos do
-		drawLight(drawOccludersFn, lightInfos[i], coordTransX, coordTransY)
+		light.drawLight(drawOccludersFn, lightInfos[i], coordTransX, coordTransY)
 	end
 end
 
 ------------------
 -- PRIVATE DATA --
 ------------------
+local lightRenderShader, shadowMapShader
 
-function drawLight(drawOccludersFn, lightInfo, coordTransX, coordTransY)
-	lightInfo.occludersCanvas:renderTo(function() love.graphics.clear() end)
-	lightInfo.shadowMapCanvas:renderTo(function() love.graphics.clear() end)
-	lightInfo.lightRenderCanvas:renderTo(function() love.graphics.clear() end)
+function light.drawLight(drawOccludersFn, lightInfo, coordTransX, coordTransY)
+	lightInfo.occludersCanvas:renderTo(function() LG.clear() end)
+	lightInfo.shadowMapCanvas:renderTo(function() LG.clear() end)
+	lightInfo.lightRenderCanvas:renderTo(function() LG.clear() end)
 
 	lightRenderShader:send("xresolution", lightInfo.size);
 	shadowMapShader:send("yresolution", lightInfo.size);
 
 	-- Upper-left corner of light-casting box.
-	x = lightInfo.x - (lightInfo.size / 2) + coordTransX
-	y = lightInfo.y - (lightInfo.size / 2) + coordTransY
+	local x = lightInfo.x - (lightInfo.size / 2) + coordTransX
+	local y = lightInfo.y - (lightInfo.size / 2) + coordTransY
 
 	-- Translating the occluders by the position of the light-casting
 	-- box causes only occluders in the box to appear on the canvas.
-	love.graphics.push()
-	love.graphics.translate(-x, -y)
+	LG.push()
+	LG.translate(-x, -y)
 	lightInfo.occludersCanvas:renderTo(drawOccludersFn)
-	love.graphics.pop()
+	LG.pop()
 
 	-- We need to un-apply any scrolling coordinate translation, because
 	-- we want to draw the light/shadow effect canvas (and helpers) literally at
 	-- (0, 0) on the screen. This didn't apply to the occluders because occluders
 	-- on screen should be affected by scrolling translation.
-	love.graphics.push()
-	love.graphics.translate(-coordTransX, -coordTransY)
+	LG.push()
+	LG.translate(-coordTransX, -coordTransY)
 
-	love.graphics.setShader(shadowMapShader)
-	love.graphics.setCanvas(lightInfo.shadowMapCanvas)
-	love.graphics.draw(lightInfo.occludersCanvas, 0, 0)
-	love.graphics.setCanvas()
-	love.graphics.setShader()
+	LG.setShader(shadowMapShader)
+	LG.setCanvas(lightInfo.shadowMapCanvas)
+	LG.draw(lightInfo.occludersCanvas, 0, 0)
+	LG.setCanvas()
+	LG.setShader()
 
-	love.graphics.setShader(lightRenderShader)
-	love.graphics.setCanvas(lightInfo.lightRenderCanvas)
-	love.graphics.draw(lightInfo.shadowMapCanvas, 0, 0, 0, 1, lightInfo.size)
-	love.graphics.setCanvas()
-	love.graphics.setShader()
+	LG.setShader(lightRenderShader)
+	LG.setCanvas(lightInfo.lightRenderCanvas)
+	LG.draw(lightInfo.shadowMapCanvas, 0, 0, 0, 1, lightInfo.size)
+	LG.setCanvas()
+	LG.setShader()
 
 
-	love.graphics.setBlendMode("add")
-	love.graphics.setColor(lightInfo.r, lightInfo.g, lightInfo.b, 255)
-	love.graphics.draw(lightInfo.lightRenderCanvas, x, y + lightInfo.size, 0, 1, -1)
-	love.graphics.setBlendMode("alpha")
+	LG.setBlendMode("add")
+	LG.setColor(lightInfo.r, lightInfo.g, lightInfo.b, 255)
+	LG.draw(lightInfo.lightRenderCanvas, x, y + lightInfo.size, 0, 1, -1)
+	LG.setBlendMode("alpha")
 
-	love.graphics.pop()
+	LG.pop()
 end
 
 
 -- Shader for caculating the 1D shadow map.
-shadowMapShader = love.graphics.newShader([[
+shadowMapShader = LG.newShader([[
 	#define PI 3.14
 	extern number yresolution;
 	const float ALPHA_THRESHOLD = 0.01;
-
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
 		number distance = 1.0;
-		
+
 		// Iterate through the occluder map's y-axis.
 		for (number y = 0.0; y < yresolution; y++) {
 			// Rectangular to polar
 			vec2 norm = vec2(texture_coords.s, y / yresolution) * 2.0 - 1.0;
 			number theta = PI * 1.5 + norm.x * PI; 
 			number r = (1.0 + norm.y) * 0.5;
-
 			//coord which we will sample from occlude map
 			vec2 coord = vec2(-r * sin(theta), -r * cos(theta)) / 2.0 + 0.5;
-	
+
 			//sample the occlusion map
 			vec4 data = Texel(texture, coord);
-	
+
 			//the current distance is how far from the top we've come
 			number dst = y / yresolution;
-	
+
 			//if we've hit an opaque fragment (occluder), then get new distance
 			//if the new distance is below the current, then we'll use that for our ray
 			number caster = data.a;
@@ -124,22 +127,19 @@ shadowMapShader = love.graphics.newShader([[
 				break;
 				// NOTE: we could probably use "break" or "return" here
 			}
-
 		}
 		return vec4(vec3(distance), 1.0);
 	}
 ]])
 
 -- Shader for rendering blurred lights and shadows.
-lightRenderShader = love.graphics.newShader([[
+lightRenderShader = LG.newShader([[
 	#define PI 3.14
 	extern number xresolution;
-
 	//sample from the 1D distance map
 	number sample(vec2 coord, number r, Image u_texture) {
 		return step(r, Texel(u_texture, coord).r);
 	}
-
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
 		// Transform rectangular to polar coordinates.
 		vec2 norm = texture_coords.st * 2.0 - 1.0;
@@ -176,3 +176,6 @@ lightRenderShader = love.graphics.newShader([[
 	 	return vec4(vec3(1.0), sum * smoothstep(1.0, 0.0, r));
 	}
 ]])
+
+
+return light
